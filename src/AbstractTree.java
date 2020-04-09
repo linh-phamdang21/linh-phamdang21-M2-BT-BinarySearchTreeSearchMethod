51d564c66847695aae44ba9523e89ad934c10277
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-04-09 17:23:19 +0700

    Add files

---
+++ src/AbstractTree.java
@@ -0,0 +1,13 @@
+public abstract class AbstractTree<E> {
+    @Override
+    public void inorder() {
+    }
+
+    @Override
+    public void preOrder() {
+    }
+
+    @Override
+    public void postOrder() {
+    }
+}
---
+++ src/BinarySearchTree.java
@@ -0,0 +1,103 @@
+public class BinarySearchTree<E extends Comparable<E>> extends AbstractTree<E> {
+    protected TreeNode<E> root;
+    protected int size = 0;
+
+    public BinarySearchTree() {
+    }
+
+    public BinarySearchTree(E[] objects) {
+        for (int i = 0; i < objects.length; i++) {
+            insert(objects[i]);
+        }
+    }
+
+    @Override
+    public boolean insert(E e) {
+        if (root == null) {
+            root = createNewNode(e);
+        } else {
+            TreeNode<E> parent = null;
+            TreeNode<E> current = root;
+            while (current != null) {
+                if (e.compareTo(current.element) < 0) {
+                    parent = current;
+                    current = current.left;
+                } else if (e.compareTo(current.element) > 0) {
+                    parent = current;
+                    current = current.right;
+                } else {
+                    return false;
+                }
+            }
+            if (e.compareTo(parent.element) < 0) {
+                parent.left = createNewNode(e);
+            } else {
+                parent.right = createNewNode(e);
+            }
+        }
+        size++;
+        return true;
+    }
+
+    protected TreeNode<E> createNewNode(E e) {
+        return new TreeNode<>(e);
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public boolean find(TreeNode<E> treeNode, E e) {
+        if (treeNode == null) {
+            return false;
+        }
+        if (treeNode.element.equals(e)) {
+            return true;
+        }
+        return e.compareTo(treeNode.element) < 0 ? find(treeNode.left, e) : find(treeNode.right, e);
+    }
+
+    @Override
+    public void inorder() {
+        inorder(this.root);
+    }
+
+    protected void inorder(TreeNode<E> root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.element + " ");
+        inorder(root.right);
+    }
+
+    @Override
+    public void preOrder() {
+        preOrder(this.root);
+    }
+
+    protected void preOrder(TreeNode<E> root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.element + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    @Override
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    protected void postOrder(TreeNode<E> root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.element + " ");
+    }
+}
